cd0f7a3710adcc6b02311cb91261c361a0688985
Two-Jay <djeeee1272@gmail.com>
2021-06-11 22:31:08 +0900

    client validate check complete

--- client/client.c
+++ client/client.c
@@ -6,14 +6,29 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 04:42:20 by jekim             #+#    #+#             */
-/*   Updated: 2021/06/11 19:48:16 by jekim            ###   ########.fr       */
+/*   Updated: 2021/06/11 22:30:36 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./client.h"
 
+int ft_validate_input(argc, argv, int *pid)
+{
+	if (argc != 3)
+		ft_strerr("Usage");
+	if (argv[2][0])
+		ft_strerr("invalid msg");
+	*pid = ft_atoi(argv[1]);
+	if (*pid < 0)
+		ft_strerr("invalid pid");
+}
+
 int main(int argc, char **argv)
 {
-	ft_putendl_fd("hello, client", 1);
+	int server_pid;
+	int client_pid;
+
+	client_pid = getpid();
+	ft_validate_input(argc, argv, &server_pid);
 	return (0);
 }
